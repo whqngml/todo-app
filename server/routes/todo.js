6a0b6d62b5a6134d36857fcc9132357144ec618c
whqngml <archangel67@naver.com>
2022-12-12 12:25:12 +0900

    feat: patch, delete 기능 구현

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -38,4 +38,52 @@ router.post("/todo", async (req, res) => {
   }
 });
 
+// PATCH localhost:PORT/todo/:todoId - edit a specific todo (UPDATE)
+// 수정 성공시 true -> res.send(true)
+// 수정 실패시 false -> res.send(false)
+router.patch("/todo/:todoId", async (req, res) => {
+  try {
+    let patchTodo = await Todo.update(
+      {
+        title: req.body.title,
+        done: req.body.done,
+      },
+      {
+        where: {
+          id: req.params.todoId,
+        },
+      }
+    );
+    console.log(req.params);
+    console.log(req.body);
+    console.log(patchTodo);
+    if (patchTodo == 0) {
+      res.send(false);
+    } else {
+      res.send(true);
+    }
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+router.delete("/todo/:todoId", async (req, res) => {
+  try {
+    let deleteTodo = await Todo.destroy({
+      where: {
+        id: req.params.todoId,
+      },
+    });
+    console.log(deleteTodo);
+    console.log(req.params);
+    if (deleteTodo == 0) {
+      res.send("삭제실패");
+    } else {
+      res.send("삭제성공");
+    }
+  } catch (err) {
+    res.send(err);
+  }
+});
+
 module.exports = router;
