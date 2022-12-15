1c7dd9ad7a28637d97e7059ab8a63e8b42098e28
whqngml <archangel67@naver.com>
2022-12-15 09:58:13 +0900

    feat: READ API 연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,55 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import axios from "axios";
 import AddTodo from "./components/AddTodo";
 import Todo from "./components/Todo";
 import "../src/styles/common.scss";
 
 const App = () => {
-  const [todoItems, setTodoItems] = useState([
-    {
-      id: 1,
-      title: "My Todo1",
-      done: false,
-    },
-    {
-      id: 2,
-      title: "My Todo2",
-      done: false,
-    },
-    {
-      id: 3,
-      title: "My Todo3",
-      done: true,
-    },
-  ]);
+  const [todoItems, setTodoItems] = useState([]);
   const todoId = useRef(4);
+
+  useEffect(() => {
+    console.log("첫 렌더링 완료!");
+
+    const getTodos = async () => {
+      let result = await axios.get("http://localhost:8080/todos");
+      console.log(result);
+      setTodoItems(result.data);
+    };
+    getTodos();
+  }, []);
 
   // AddTodo 컴포넌트는 상위 컴포넌트(App)의 todoItems(state)에 접근 불가능
   // 상위 컴포넌트(App)은 AddTodo 컴포넌트 접근 가능
   // => App 컴포넌트에 addItem() 함수를 정의하고, 해당 함수를 AddTodo props로 넘겨야 함
   const addItem = (newItem) => {
+    const postTodo = async () => {
+      let create = await axios.post("http://localhost:8080/todo", {
+        title: newItem.title,
+      });
+      console.log("창조", create.data);
+      return setTodoItems([...todoItems, create.data]); // setTodoItems(todoItems.concat(newItem))
+    };
     // newItem - {id: xx, title: xx, done: false}
-    newItem.id = todoId.current++; // key를 위한 id 설정
-    newItem.done = false; // done 초기화
+    // newItem.id = todoId.current++; // key를 위한 id 설정
+    // newItem.done = false; // done 초기화
     // 기존 todoItems를 유지하고, 새로운 newItem을 추가
-    setTodoItems([...todoItems, newItem]); // setTodoItems(todoItems.concat(newItem))
+    postTodo();
   };
 
   const deleteItem = (targetItem) => {
     const newTodoItems = todoItems.filter((item) => item.id !== targetItem.id);
     console.log(newTodoItems);
     setTodoItems(newTodoItems);
+    // const deleteTodo = async () => {
+    //   let destroy = await axios.post("http://localhost:8080/todo/:todoId", {
+    //     title: newItem.title,
+    //   });
+    //   console.log("삭제", destroy.data);
+    //   return setTodoItems([...todoItems, destroy.data]); // setTodoItems(todoItems.concat(newItem))
+    // };
+
+    // deleteTodo();
   };
 
   return (
@@ -47,7 +59,7 @@ const App = () => {
       <div className="left-todos">❤️ {todoItems.length} Todos</div>
       {todoItems.length > 0 ? (
         todoItems.map((item) => {
-          // console.log(item); // {id: 1, title: 'My Todo1', done: false}
+          console.log(item); // {id: 1, title: 'My Todo1', done: false}
           return <Todo key={item.id} item={item} deleteItem={deleteItem} />;
         })
       ) : (
--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 8080;
 const todoRouter = require("./routes/todo");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors()); // 모든 서버에서 보내는 요청 수락
 app.use("/", todoRouter); // 기본주소: localhost:PORT/
 
 app.listen(PORT, () => {
